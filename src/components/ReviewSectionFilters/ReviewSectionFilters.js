9177f792a0c0183abb5d99b9516ed499224ce6e0
Jesse <jesse.rogers@uconn.edu>
2020-08-05 16:57:03 -0400

    reviewSection layout buttons

--- src/components/ReviewSectionFilters/ReviewSectionFilters.js
+++ src/components/ReviewSectionFilters/ReviewSectionFilters.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import styles from './ReviewSectionFilters.module.scss';
 
-const ReviewSectionFilters = () => {
+const ReviewSectionFilters = ({
+  rowLayout,
+  onRowLayoutChange,
+  onCardLayoutChange,
+}) => {
   return (
     <div className={styles.reviewSection}>
       <input type="text" placeholder="Search by review or category..." />
 
       <div>
-        <button>
+        <button
+          className={rowLayout ? null : styles.active}
+          onClick={onCardLayoutChange}
+        >
           <span className="far fa-window-maximize"></span>
         </button>
-        <button>
+        <button
+          className={rowLayout ? styles.active : null}
+          onClick={onRowLayoutChange}
+        >
           <span className="fas fa-bars"></span>
         </button>
       </div>
--- src/pages/Reviews/Reviews.js
+++ src/pages/Reviews/Reviews.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Reviews.module.scss';
 import NoReviewsMessage from '../../components/NoReviewsMessage/NoReviewsMessage';
 import ReviewCard from '../../components/ReviewCard/ReviewCard';
 import ReviewSectionFilters from '../../components/ReviewSectionFilters/ReviewSectionFilters';
 
 const Reviews = ({ reviews }) => {
+  const [rowLayout, setRowLayout] = useState(false);
+  const layoutClass = rowLayout ? styles.rows : styles.cards;
+
+  const handleRowLayoutChange = () => {
+    setRowLayout(true);
+  };
+
+  const handleCardLayoutChange = () => {
+    setRowLayout(false);
+  };
+
   return (
     <section className={styles.reviewSection}>
       <header>
-        <ReviewSectionFilters />
+        <ReviewSectionFilters
+          rowLayout={rowLayout}
+          onRowLayoutChange={handleRowLayoutChange}
+          onCardLayoutChange={handleCardLayoutChange}
+        />
       </header>
-      <div className={styles.reviewSection__content}>
+      <div className={`${styles.reviewSection__content} ${layoutClass}`}>
         {reviews.length ? (
           reviews.map((review) => (
             <ReviewCard review={review} key={review.id} />
